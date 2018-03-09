c8b086fe80ddbd870717fa021bc4d0af3ce7c329
Carlton Yeung <cayeung@cs.stonybrook.edu>
2018-03-09 11:04:56 -0500

    Remove main.css so we have super minimal styling; Make sure csrf token is in every jQuery ajax request; Implement a basic adduser form

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,24 +2,37 @@
 
 @section('title', 'twitir')
 
-@section('left')
-<div id='center-main'>
-    <h1>twitir</h1>
-</div>
+@section('content')
+<form id='adduser' method='POST'>
+	@csrf
+  <input type='text' name='username' placeholder='Name'>
+  <input type='password' name='password' placeholder='Password'>
+  <input type='email' name='email' placeholder='Email'>
+  <input type='submit' value='/adduser'>
+</form>
 @endsection
 
-@section('center')
-@endsection
-
-@section('right')
-@endsection
-
-@section('page-style')
+@section('style')
 <style>
 </style>
 @endsection
 
-@section('page-script')
+@section('script')
 <script>
+	$('#adduser').submit(function() {
+		// Get the form data
+		var dataArray = $(this).serializeArray();
+
+		// Convert array to object with the form data
+		var dataObj = {};
+		for (var i = 0; i < dataArray.length; i++) {
+			dataObj[dataArray[i].name] = dataArray[i].value;
+		}
+
+		console.log(JSON.stringify(dataObj));
+
+		// Prevent html form from being submitted and the page refreshing
+		return false;
+	});
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,31 +3,29 @@
     <head>
         <title>@yield('title')</title>
 
-        <!-- Without this line, POST will produce HTTP 419 error -->
+        <!-- https://laravel.com/docs/5.6/csrf -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body>
-        <div id='left-center-right-grid'>
-            <div id='left'>
-                @yield('left')
-            </div>
-            <div id='center'>
-                @yield('center')
-            </div>
-            <div id='right'>
-                @yield('right')
-            </div>
-        </div>
+        <h1>twitir</h1>
+        @yield('content')
     </body>
 
-    <!-- STYLES -->
+    <!-- styles -->
     <!-- Bootstrap -->
     <link rel='stylesheet' type = 'text/css' href='{{ URL::asset('css/app.css') }}'>
-    <link rel='stylesheet' type = 'text/css' href='{{ URL::asset('css/main.css') }}'>
-    @yield('page-style')
+    @yield('style')
 
-    <!-- SCRIPTS -->
-    <!-- jQuery -->
+    <!-- scripts -->
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-    @yield('page-script')
+
+    <!-- https://laravel.com/docs/5.6/csrf -->
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    @yield('script')
 </html>
