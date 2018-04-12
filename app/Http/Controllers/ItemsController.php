2e38fb8cf5b3d0bc89b29dc315b19370e72007fc
Carlton Yeung <carlton.yeung@stonybrook.edu>
2018-04-12 11:16:39 -0400

    Need parentheses?

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -68,7 +68,7 @@ class ItemsController extends Controller
                 'filled',
             ],
             'media.*' => [
-                'regex:^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
+                'regex:(^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$)',
             ],
         ]);
 
