3d95d7e2df82d0bb42fa9f53e981e42e68bd6adf
Carlton Yeung <carlton.yeung@stonybrook.edu>
2018-04-12 11:28:01 -0400

    Don't allow duplicate media IDs

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -69,6 +69,7 @@ class ItemsController extends Controller
             ],
             'media.*' => [
                 'regex:(^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$)',
+                'distinct',
             ],
         ]);
 
